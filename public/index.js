c63db544bf2701db4ac3d72119ee3515adcc1e41
Shyaboi <ian.shane.sears@gmail.com>
2020-08-03 12:17:46 -0700

    added save scroll place on page load

--- public/index.js
+++ public/index.js
@@ -12,3 +12,25 @@ $(function() {
         }
     });
 });
+
+var scrollPosition
+
+$(window).on('scroll', function() {
+  scrollPosition= $(this).scrollTop()
+   console.log(scrollPosition) 
+   return scrollPosition
+});
+console.log(scrollPosition)
+$(function() {
+    $(window).unload(function() {
+        
+       localStorage.setItem("scrollPosition", scrollPosition);
+    });
+    if(localStorage.scrollPosition) {
+       $(scrollPosition).scrollTop(localStorage.getItem("scrollPosition"));
+    }
+ });
+
+ $(document).ready(function(){
+    $(this).scrollTop(localStorage.getItem("scrollPosition"));
+});
